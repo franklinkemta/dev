c8ce06a0cc25df30a17a993a1ae61c02e765b4ac
franklinkemta <franklinkemta@gmail.com>
2025-05-13 04:38:20 -0600

    Fix domain CNAME override

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://dev.franklinkemta.com',
+  customFields: {
+    cname: 'dev.franklinkemta.com',
+  },
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
